eb1bafe4e54fabafcde33e581af344f7a29cb9a5
yosifMohamedain <63748976+Yousif-M-M-H@users.noreply.github.com>
2023-05-06 16:04:37 +0200

    second Commit

--- lib/pages/shipments/shipmentForm.dart
+++ lib/pages/shipments/shipmentForm.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:speedyship/pages/shipments/showShipments.dart';
 import '../../components/my_textfield.dart';
 import '../../components/dropdown_form_field.dart';
 import '../../components/city_dropdown_form_field.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ShipmentForm extends StatefulWidget {
   ShipmentForm({Key? key}) : super(key: key);
@@ -12,7 +14,7 @@ class ShipmentForm extends StatefulWidget {
 }
 
 class _ShipmentFormState extends State<ShipmentForm> {
-  String _selectedOption = 'food'; // default option is 'food'
+  String _selectedOption = 'food';
   String _citysSelectedOption = "magusa";
 
   final weightController = TextEditingController();
@@ -27,10 +29,13 @@ class _ShipmentFormState extends State<ShipmentForm> {
 
   final destinationcontroller = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<void> _submitForm() async {
     if (_formKey.currentState != null && _formKey.currentState!.validate()) {
       try {
+        final user = _auth.currentUser;
+        final userId = user?.uid;
         await FirebaseFirestore.instance.collection('shipments').add({
           'category': _selectedOption,
           'weight': int.tryParse(weightController.text) ?? 0,
@@ -40,22 +45,15 @@ class _ShipmentFormState extends State<ShipmentForm> {
           'city': _citysSelectedOption,
           'location': locationController.text,
           'destination': destinationcontroller.text,
+          'userId': userId,
         });
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Shipment added successfully')),
         );
-        // clear form data after submission
-        _selectedOption = 'food';
-        _citysSelectedOption = 'magusa';
-        weightController.clear();
-        lengthController.clear();
-        widthController.clear();
-        heightController.clear();
-        locationController.clear();
-        destinationcontroller.clear();
+
         setState(() {});
         // Go back to previous screen
-        Navigator.pop(context);
+        // Navigator.pop(context);
       } catch (error) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Failed to add shipment')),
@@ -65,101 +63,187 @@ class _ShipmentFormState extends State<ShipmentForm> {
     }
   }
 
+  String? _validateText(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'This field is required';
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("shipment details")),
-      body: ListView(
-        children: [
-          Column(
-            children: [
-              Text(
-                "shipment details",
-                style: TextStyle(fontSize: 25),
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              MyDropdownButtonFormField(
-                selectedOption: _selectedOption,
-                onChanged: (value) {
-                  setState(() {
-                    _selectedOption = value.toString();
-                  });
-                },
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              MyTextField(
-                controller: weightController,
-                hintText: 'weight',
-                obscureText: false,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              MyTextField(
-                controller: lengthController,
-                hintText: 'length',
-                obscureText: false,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              MyTextField(
-                controller: widthController,
-                hintText: 'width',
-                obscureText: false,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              MyTextField(
-                controller: heightController,
-                hintText: 'height',
-                obscureText: false,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                "Shipping Address",
-                style: TextStyle(fontSize: 25),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              MyCityDropdownButtonFormField(
-                selectedOption: _citysSelectedOption,
-                onChanged: (value) {
-                  setState(() {
-                    _citysSelectedOption = value.toString();
-                  });
-                },
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              MyTextField(
-                  controller: locationController,
-                  hintText: 'Location',
-                  obscureText: false),
-              SizedBox(
-                height: 20,
-              ),
-              MyTextField(
-                  controller: destinationcontroller,
-                  hintText: 'destination',
-                  obscureText: false),
-              ElevatedButton(
-                  onPressed: () {
-                    _submitForm();
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          children: [
+            Column(
+              children: [
+                Text(
+                  "shipment details",
+                  style: TextStyle(fontSize: 25),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                MyDropdownButtonFormField(
+                  selectedOption: _selectedOption,
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedOption = value.toString();
+                    });
                   },
-                  child: Text("Submit"))
-            ],
-          ),
-        ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: weightController,
+                  decoration: InputDecoration(
+                    labelText: 'Weight',
+                    hintText: 'Enter weight',
+                  ),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'This field is required';
+                    }
+                    if (double.tryParse(value) == null) {
+                      return 'Please enter a valid number';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: lengthController,
+                  decoration: InputDecoration(
+                    labelText: 'length',
+                    hintText: 'Enter length',
+                  ),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'This field is required';
+                    }
+                    if (double.tryParse(value) == null) {
+                      return 'Please enter a valid number';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: widthController,
+                  decoration: InputDecoration(
+                    labelText: 'width',
+                    hintText: 'Enter width',
+                  ),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'This field is required';
+                    }
+                    if (double.tryParse(value) == null) {
+                      return 'Please enter a valid number';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: heightController,
+                  decoration: InputDecoration(
+                    labelText: 'height',
+                    hintText: 'Enter height',
+                  ),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'This field is required';
+                    }
+                    if (double.tryParse(value) == null) {
+                      return 'Please enter a valid number';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Shipping Address",
+                  style: TextStyle(fontSize: 25),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                MyCityDropdownButtonFormField(
+                  selectedOption: _citysSelectedOption,
+                  onChanged: (value) {
+                    setState(() {
+                      _citysSelectedOption = value.toString();
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: locationController,
+                  decoration: InputDecoration(
+                    labelText: 'location Name',
+                    hintText: 'Enter location name',
+                  ),
+                  validator: _validateText,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: destinationcontroller,
+                  decoration: InputDecoration(
+                    labelText: 'destination Name',
+                    hintText: 'Enter desintation name',
+                  ),
+                  validator: _validateText,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState != null &&
+                        _formKey.currentState!.validate()) {
+                      _submitForm();
+
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => ShipmentList(),
+                        ),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('shipment added')));
+                      _selectedOption = 'food';
+                      _citysSelectedOption = 'magusa';
+                      weightController.clear();
+                      lengthController.clear();
+                      widthController.clear();
+                      heightController.clear();
+                      locationController.clear();
+                      destinationcontroller.clear();
+                    }
+                  },
+                  child: Text('Submit'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
